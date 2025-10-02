1f30db435245a361e4161407f4e91c657efa7db6
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-10-02 10:05:28 +0200

    Squash merge fix_dia into dev

---
+++ src/client/CMakeFiles/client.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "../../bin/client"
+  "../../bin/client.pdb"
+  "CMakeFiles/client.dir/link.d"
+  "CMakeFiles/client.dir/main.cpp.o"
+  "CMakeFiles/client.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/client.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
