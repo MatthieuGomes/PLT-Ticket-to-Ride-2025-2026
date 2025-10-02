4a8bcae8db74a75dea8d80f76747691a90a6ac6b
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-10-02 10:05:28 +0200

    Squash merge fix_dia into dev

---
+++ run_test.sh
@@ -0,0 +1,1 @@
+cd "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test" && ctest --timeout 300 ||/bin/true
---
+++ test/shared/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/shared
+# Build directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/shared
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_shared_dummy "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/shared/test_shared_dummy")
+set_tests_properties(test_shared_dummy PROPERTIES  WORKING_DIRECTORY "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026" _BACKTRACE_TRIPLES "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/CMakeLists.txt;77;add_test;/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/shared/CMakeLists.txt;2;add_custom_test;/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/shared/CMakeLists.txt;0;")
