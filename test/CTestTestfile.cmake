ecb982f665ccbcb77384ef682cccaaf3674fadae
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-10-09 10:56:44 +0200

    Fixing sharing issue of cmake project

--- test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
- CMake generated Testfile for
-# Source directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test
-# Build directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("shared")
-subdirs("client")
--- test/client/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/client
-# Build directory: /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/client
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_client_dummy "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/client/test_client_dummy")
-set_tests_properties(test_client_dummy PROPERTIES  WORKING_DIRECTORY "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026" _BACKTRACE_TRIPLES "/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/CMakeLists.txt;77;add_test;/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/client/CMakeLists.txt;3;add_custom_test;/home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/test/client/CMakeLists.txt;0;")
