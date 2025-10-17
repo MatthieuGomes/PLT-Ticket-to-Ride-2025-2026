6fc61bddf08eb72df1a5a88c1fb53a674c03fc2e
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-10-17 09:35:26 +0200

    compatibility and demo client + state

---
+++ src/client/client/Client.cpp
@@ -0,0 +1,14 @@
+#include "Client.h"
+#include <iostream>
+
+namespace client
+{
+    void Client::printState()
+    {
+    }
+
+    void Client::helloWorld()
+    {
+        std::cout << "HelloWorld" << std::endl;
+    }
+}
--- src/client/main.cpp
+++ src/client/main.cpp
@@ -2,6 +2,11 @@
 
 // The following lines are here to check that SFML is installed and working
 #include <SFML/Graphics.hpp>
+#include <string.h>
+
+#include "client.h"
+#include "state/State.h"
+
 
 void testSFML() {
     sf::Texture texture;
@@ -13,9 +18,23 @@ using namespace std;
 
 int main(int argc,char* argv[])
 {
+    client::Client client;
+    state::State state;
+    if(argc<1){
+        cout << "No arguments provided." << endl;
+        return EXIT_FAILURE;
+    }
+    client = client::Client();
+    state = state::State();
 
-
-    cout << "HelloWorld" << endl;
-
-    return 0;
+    if(strcmp(argv[1], "hello") == 0) {
+        client.helloWorld();
+        return EXIT_SUCCESS;
+    } else if(strcmp(argv[1], "state") == 0) {
+        client.printState();
+        return EXIT_SUCCESS;
+    } else {
+        cout << "Unknown argument: " << argv[1] << endl;
+        return EXIT_FAILURE;
+    }
 }
