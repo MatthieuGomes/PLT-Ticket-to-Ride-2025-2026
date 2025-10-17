d083ec3813bbc05e1f8eddd143553b4245658909
YosraGuesmi <guesmiyosra0@gmail.com>
2025-10-17 10:21:30 +0200

    deleting state.dia

--- src/shared/CMakeFiles/shared_static.dir/state/player/player.cpp.o.d
+++
@@ -1,3 +0,0 @@
-src/shared/CMakeFiles/shared_static.dir/state/player/player.cpp.o: \
- /home/matthieu/Desktop/PLT_Ticket_to_Ride_2025-2026/src/shared/state/player/player.cpp \
- /usr/include/stdc-predef.h
