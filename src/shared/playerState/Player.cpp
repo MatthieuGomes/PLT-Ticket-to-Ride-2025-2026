b4ce0d7b2519b2838e2529fa01f16dd2e188f09c
YosraGuesmi <guesmiyosra0@gmail.com>
2025-10-17 10:39:18 +0200

    adding getters & setters

--- src/shared/playerState/Player.cpp
+++ src/shared/playerState/Player.cpp
@@ -1,3 +1,49 @@
-//
-// Created by yosra on 10/17/25.
-//
+#include "Player.h"
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+using namespace playerState;
+
+// ----- Getters & Setters -----
+
+int Player::getId() {
+    return id;
+}
+
+void Player::setId(int id) {
+    this->id = id;
+}
+
+std::string Player::getName() {
+    return name;
+}
+
+void Player::setName(std::string name) {
+    this->name = name;
+}
+
+int Player::getScore() {
+    return score;
+}
+
+void Player::setScore(int score) {
+    this->score = score;
+}
+
+int Player::getNbStations() {
+    return nbStations;
+}
+
+void Player::setNbStations(int nbStations) {
+    this->nbStations = nbStations;
+}
+
+int Player::getNbRoads() {
+    return nbRoads;
+}
+
+void Player::setNbRoads(int nbRoads) {
+    this->nbRoads = nbRoads;
+}
+
