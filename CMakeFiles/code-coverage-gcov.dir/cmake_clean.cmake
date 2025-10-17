d35546e25c3bf97494e9078420f48d05d8ef540e
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-10-17 09:23:56 +0100

    delete state.dia

--- CMakeFiles/code-coverage-gcov.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/code-coverage-gcov"
-  "code-coverage-gcov.xml"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/code-coverage-gcov.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
