6a12d868e2d7c9b2ddba04ed1dac8bfaa5b73938
YosraGuesmi <guesmiyosra0@gmail.com>
2025-10-19 23:33:22 +0200

    [players_v1_final] Implement printPlayersState method temporary  #34  DONE

--- src/shared/playersState/PlayersState.cpp
+++ src/shared/playersState/PlayersState.cpp
@@ -1,12 +1,46 @@
 #include "PlayersState.h"
+#include <iostream>
+
+using namespace std;
 
 namespace playersState {
 
-  /// class CardsState - 
-  PlayersState::PlayersState () {
-  }
-  void PlayersState::printPlayersState () {
-      // Here @yosra will implement the details to print your part of the players state
-  }
+    // ----- Constructor -----
+    PlayersState::PlayersState() {
+        // Nothing to initialize since there is no player list in the header
+    }
+
+    // ----- Print Players State -----
+    void PlayersState::printPlayersState() {
+        cout << "======= Players State =======" << endl;
+
+        // Since PlayersState has no player list, we create temporary test players for display
+        Player p1;
+        p1.setId(1);
+        p1.setName("Alice");
+        p1.setNbStations(2);
+        p1.setNbRoads(3);
+
+        Player p2;
+        p2.setId(2);
+        p2.setName("Bob");
+        p2.setNbStations(1);
+        p2.setNbRoads(4);
+
+        // Display info for each player
+        cout << "-----------------------------" << endl;
+        cout << "Player ID      : " << p1.getId() << endl;
+        cout << "Name           : " << p1.getName() << endl;
+        cout << "Stations owned : " << p1.getNbStations() << endl;
+        cout << "Roads claimed  : " << p1.getNbRoads() << endl;
+
+        cout << "-----------------------------" << endl;
+        cout << "Player ID      : " << p2.getId() << endl;
+        cout << "Name           : " << p2.getName() << endl;
+        cout << "Stations owned : " << p2.getNbStations() << endl;
+        cout << "Roads claimed  : " << p2.getNbRoads() << endl;
+
+        cout << "=============================" << endl;
+    }
 
 };
