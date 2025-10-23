c86c6395ceafb7f897738d955a330d07cd210f56
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-10-23 08:20:21 +0100

    Card state implementation #8

--- src/shared/cardsState/CardsState.cpp
+++ src/shared/cardsState/CardsState.cpp
@@ -1,12 +1,19 @@
 #include "CardsState.h"
 
+#include <iostream>
+
 namespace cardsState {
 
   /// class CardsState - 
   CardsState::CardsState () {
   }
   void CardsState::printCardsState () {
-      // Here @chahrazed will implement the details to print your part of the cards state
-  }
+      std::cout << " Current Cards State " << std::endl;
 
-};
+      visibleDeck.displayDeck();
+      wagonDeck.displayDeck();
+      destinationDeck.displayDeck();
+      discardedDeck.displayDeck();
+
+      std::cout << "Number of player hands: " << playerHands.size() << std::endl;
+  }
