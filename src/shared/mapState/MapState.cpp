1b05a27532d147591051c1195a382bf96acc167d
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-11-06 12:49:57 +0000

    [map_v1_final_fix] fixing mapstate generation

--- src/shared/mapState/MapState.cpp
+++ src/shared/mapState/MapState.cpp
@@ -13,7 +13,7 @@
 namespace mapState {
 
   MapState::MapState(){
-      this->gameGraph = boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS, mapState::Station*, mapState::Road*>();
+      this->gameGraph = boost::adjacency_list<>();
 
       StationData* parisData  = new StationData{"Paris",  1};
       StationData* berlinData = new StationData{"Berlin", 2};
@@ -40,44 +40,21 @@ namespace mapState {
 
       Roads = {roadPB, roadBR, roadPM, roadMR};
 
-      // fill graph bundles
-      for (Station* s : Stations) gameGraph[s->vertex] = s;
-      for (Road* r : Roads)    gameGraph[r->edge]   = r;
-
   };
-/*
-  MapState* MapState::generateMap() {
-      // hardcoded stations 
-      StationData* parisData  = new StationData{"Paris",  1};
-      StationData* berlinData = new StationData{"Berlin", 2};
-      StationData* madridData = new StationData{"Madrid", 3};
-      StationData* romeData   = new StationData{"Rome",   4};
-
-      Station* paris  = new Station{add_vertex(gameGraph), parisData};
-      Station* berlin = new Station{add_vertex(gameGraph), berlinData};
-      Station* madrid = new Station{add_vertex(gameGraph), madridData};
-      Station* rome   = new Station{add_vertex(gameGraph), romeData};
-
-      Stations = {paris, berlin, madrid, rome};
-
-      // roads
-      RoadData* p_b = new RoadData{"P-B", "red",    4, false, {parisData,  berlinData}};
-      RoadData* b_r = new RoadData{"B-R", "blue",   3, false, {berlinData, romeData}};
-      RoadData* p_m = new RoadData{"P-M", "yellow", 5, false, {parisData,  madridData}};
-      RoadData* m_r = new RoadData{"M-R", "green",  6, false, {madridData, romeData}};
-
-      Road* roadPB = new Road{add_edge(paris->vertex,  berlin->vertex, gameGraph).first, p_b};
-      Road* roadBR = new Road{add_edge(berlin->vertex, rome->vertex,   gameGraph).first, b_r};
-      Road* roadPM = new Road{add_edge(paris->vertex,  madrid->vertex, gameGraph).first, p_m};
-      Road* roadMR = new Road{add_edge(madrid->vertex, rome->vertex,   gameGraph).first, m_r};
-
-      Roads = {roadPB, roadBR, roadPM, roadMR};
-
-      // fill graph bundles
-      for (auto* s : Stations) gameGraph[s->vertex] = s;
-      for (auto* r : Roads)    gameGraph[r->edge]   = r;
-
-      return this;
-  }*/
+    void MapState::printMapState () const {
+        std::cout << " Current Map State " << std::endl;
+        std::cout << " Stations: " << std::endl;
+        for (const Station* station : Stations) {
+            std::cout << "  - " << station->data->name << " (ID: " << station->data->ID << ")" << std::endl;
+        }
+        std::cout << " Roads: " << std::endl;
+        for (const Road* road : Roads) {
+            std::cout << "  - " << road->data->ID << " [Color: " << road->data->color
+                        << ", Length: " << road->data->length
+                        << ", Blocked: " << (road->data->isBlocked ? "Yes" : "No") << "]"
+                        << " connecting " << road->data->endpoints[0]->name
+                        << " and " << road->data->endpoints[1]->name << std::endl;
+        }
+    }
 
 };
