6517ad51c0385c731b7ebb863a4c63f8780a718a
fetohiaras <theo.farias@gmail.com>
2025-11-06 13:06:14 +0100

    [map_v1_final_fix] updating branch to current state of the work on boost

---
+++ test/shared/cardsState/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /app/test/shared/cardsState
+# Build directory: /app/test/shared/cardsState
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_cards_state "/app/test/shared/cardsState/test_cards_state")
+set_tests_properties(test_cards_state PROPERTIES  WORKING_DIRECTORY "/app" _BACKTRACE_TRIPLES "/app/test/CMakeLists.txt;77;add_test;/app/test/shared/cardsState/CMakeLists.txt;2;add_custom_test;/app/test/shared/cardsState/CMakeLists.txt;0;")
---
+++ test/shared/mapState/mapStateTests.cpp
@@ -0,0 +1,4 @@
+#define BOOST_TEST_MODULE MapTests
+
+#include <boost/test/included/unit_test.hpp>    
+#include "mapState.h"
