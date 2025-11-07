bd9be917264c605ca41b80f0cee13dfab2ffbd7b
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-11-07 16:50:44 +0100

    [cards_v1_final]   updating the sharedDeck constructor

--- src/shared/cardsState/SharedDeck.cpp
+++ src/shared/cardsState/SharedDeck.cpp
@@ -9,11 +9,14 @@
 namespace cardsState {
     // class SharedDeck -
     template <  class CardType>
-    SharedDeck<CardType>::SharedDeck () {
-        this->trash = std::make_shared<Trash<CardType>>(nullptr);
-        this->faceUpCards = std::make_shared<FaceUpCards<CardType>>(nullptr);
-        this->faceDownCards = std::make_shared<FaceDownCards<CardType>>(nullptr);
+   SharedDeck<CardType>::SharedDeck (std::vector<CardType>* trash, std::vector<CardType>* faceUpCards, std::vector<CardType>* faceDownCards){
+        this->trash = std::make_shared<Trash<CardType>>(trash);
+        this->faceUpCards = std::make_shared<FaceUpCards<CardType>>(faceUpCards);
+        this->faceDownCards = std::make_shared<FaceDownCards<CardType>>(faceDownCards);
     }
+
+
+
     template <  class CardType>
     void SharedDeck<CardType>::display() {
         std::cout << " Shared Deck Status: " << std::endl;
