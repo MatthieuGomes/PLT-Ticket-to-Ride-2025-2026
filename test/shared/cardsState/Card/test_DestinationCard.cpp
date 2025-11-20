786b7948022225b51272722fc1151afb72fe1376
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-11-20 09:36:43 +0100

    [cards_v1_final]  testing cards class

---
+++ test/shared/cardsState/Card/test_DestinationCard.cpp
@@ -0,0 +1,30 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "../../src/shared/cardsState/CardsState.h"
+#include "cardsState/DestinationCard.h"
+using namespace ::cardsState;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+  BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_CASE(TestDestinationCard) {
+  {
+    DestinationCard card("paris","rome",12);
+    BOOST_CHECK_EQUAL(card.getCityA(), "paris");
+  }
+
+  {
+    DestinationCard card("paris","rome",12);
+    BOOST_CHECK_EQUAL(card.getPoints(), 12);
+  }
+  {
+    DestinationCard card("paris","rome",12);
+    BOOST_CHECK_EQUAL(card.getCityB(), "rome");
+  }
+
+}
+
+/* vim: set sw=2 sts=2 et : */
