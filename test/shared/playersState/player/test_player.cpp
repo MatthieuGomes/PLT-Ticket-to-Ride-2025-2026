19e00550ff38ee6ef28c427770fd1531bceec567
YosraGuesmi <guesmiyosra0@gmail.com>
2025-11-20 11:15:00 +0100

    [players_v1_final] testing instantiation and getters in Player Class  with boost

--- test/shared/playersState/player/test_player.cpp
+++ test/shared/playersState/player/test_player.cpp
@@ -10,29 +10,152 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
     BOOST_CHECK(1);
 }
 
+BOOST_AUTO_TEST_CASE(TestPLayerConstructor) {
 
-BOOST_AUTO_TEST_CASE(TestPLayer)
+    cardsState::DestinationCard dest("Paris", "Marseille", 12);
+    cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+
+    std::vector<cardsState::DestinationCard> destCards = {dest};
+    std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+
+    auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+
+    Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+    BOOST_CHECK_EQUAL(player.id,1);
+    BOOST_CHECK_EQUAL(player.name,"yosra");
+    BOOST_CHECK_EQUAL(player.color,cardsState::ColorCard::RED);
+    BOOST_CHECK_EQUAL(player.score,23);
+    BOOST_CHECK_EQUAL(player.numTrains,21);
+    BOOST_CHECK_EQUAL(player.nbStations,10);
+    BOOST_CHECK_EQUAL(player.nbRoads,5);
+    BOOST_CHECK_EQUAL(player.hand,hand);
+
+}
+
+BOOST_AUTO_TEST_CASE(TestPLayergetId)
 {
     {
-        // ExampleA exA;
-        // exA.setX(53);
-        // Example ex;
-        // ex.setA(exA);
-        // BOOST_CHECK_EQUAL(ex.getA().x, 53);
-        // exA.setX(21);
-        // BOOST_CHECK_EQUAL(ex.getA().x, 53);
-        // ex.setA(exA);
-        // BOOST_CHECK_EQUAL(ex.getA().x, 21);
-    }
+        cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
 
-    {
-        // ExampleA exA;
-        // exA.setX(21);
-        // Example ex;
-        // ex.setA(exA);
-        // BOOST_CHECK_LE(ex.getA().x, 32); // Less than equal
-        // BOOST_CHECK_GT(ex.getA().x, 11); // Greater than equal
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getId(),1);
     }
 }
+
+BOOST_AUTO_TEST_CASE(TestPLayergetName)
+{
+    {
+        cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getName(), "yosra");
+
+
+    }
+}
+
+
+BOOST_AUTO_TEST_CASE(TestPlayergetColor){
+
+    {
+        cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getColor(),cardsState::ColorCard::RED);
+    }
+
+}
+
+BOOST_AUTO_TEST_CASE(TestPlayergetScore){
+
+    {   cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getScore(),23);
+    }
+
+}
+
+BOOST_AUTO_TEST_CASE(TestPlayergetNumTrains){
+
+    {   cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getNumTrains(),21);
+    }
+
+}
+
+BOOST_AUTO_TEST_CASE(TestPlayergetNbStations){
+
+    {   cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getNbStations(),10);
+    }
+
+}
+
+
+BOOST_AUTO_TEST_CASE(TestPlayergetNbRoads){
+
+    {   cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getNbRoads(),5);
+    }
+
+}
+
+
+BOOST_AUTO_TEST_CASE(TestPlayergetHand){
+
+    {   cardsState::DestinationCard dest("Paris", "Marseille", 12);
+        cardsState::WagonCard wagon(cardsState::ColorCard::RED);
+        std::vector<cardsState::DestinationCard> destCards = {dest};
+        std::vector<cardsState::WagonCard> wagonCardsVec = {wagon};
+        auto* hand = new cardsState::PlayerCards(&destCards, &wagonCardsVec);
+        Player player(1, "yosra", cardsState::ColorCard::RED, 23, 21, 10, 5, hand);
+        BOOST_CHECK_EQUAL(player.getHand(),hand);
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
 
 /* vim: set sw=2 sts=2 et : */
