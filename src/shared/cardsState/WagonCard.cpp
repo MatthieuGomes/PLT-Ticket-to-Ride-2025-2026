64748925fd86263745f2b4417b3657faf52cc486
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-11-20 09:23:36 +0100

    [cards_v1_final]  adding getter and setter for the WagonCard

--- src/shared/cardsState/WagonCard.cpp
+++ src/shared/cardsState/WagonCard.cpp
@@ -5,7 +5,13 @@
 namespace cardsState {
     WagonCard::WagonCard(ColorCard color) {
         this->color = color;
-    } ;
+    }
+    ColorCard WagonCard::getColor() {
+        return this->color;
+    }
+    void WagonCard::setColor(ColorCard color) {
+            this->color = color;
+    }
     void WagonCard::display() {
         std::cout<<" this a wagon card"<< std::endl;
         std::cout<<this->color<<std::endl;
