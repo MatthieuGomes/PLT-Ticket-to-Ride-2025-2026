37e0b869662336f6a69bbb7b2ab201cde8c19a35
fetohiaras <theo.farias@gmail.com>
2025-11-22 03:32:44 +0100

    [map_v1_final_fix] adding unit tests to mapState functions - testing getters, findShortestPath and toString

--- test/shared/mapState/test_map_state.cpp
+++ test/shared/mapState/test_map_state.cpp
@@ -33,8 +33,8 @@ BOOST_AUTO_TEST_CASE(TestExemple)
     // BOOST_CHECK_GT(ex.getA().x, 11); // Greater than equal
   }
 }
-
-BOOST_AUTO_TEST_CASE(ConstructorInitializesGraph)
+// MapState(), listStations() and listRoads() unit tests
+BOOST_AUTO_TEST_CASE(TestConstructor)
 {
     MapState map;
 
@@ -49,6 +49,84 @@ BOOST_AUTO_TEST_CASE(ConstructorInitializesGraph)
     auto roads = map.listRoads();
     BOOST_REQUIRE_EQUAL(roads.size(), 4);
 }
+
+//getStationByName()
+BOOST_AUTO_TEST_CASE(TestGetStationByName)
+{
+    MapState map;
+
+    Station* s = map.getStationByName("Berlin");
+    BOOST_REQUIRE(s != nullptr);
+    BOOST_CHECK_EQUAL(s->data->ID, 2);
+
+    // check for nonexistant station
+    BOOST_CHECK(map.getStationByName("Tokyo") == nullptr);
+}
+
+//getRoad()
+BOOST_AUTO_TEST_CASE(TestGetRoad)
+{
+    MapState map;
+
+    Station* berlin = map.getStationByName("Berlin");
+    Station* rome   = map.getStationByName("Rome");
+
+    BOOST_REQUIRE(berlin != nullptr);
+    BOOST_REQUIRE(rome   != nullptr);
+
+    Road* r = map.getRoad(berlin, rome);
+    BOOST_REQUIRE(r != nullptr);
+    BOOST_CHECK_EQUAL(r->data->ID, "B-R");
+
+    // nonexistant
+    Station* madrid = map.getStationByName("Madrid");
+    Road* none = map.getRoad(berlin, madrid);
+
+    BOOST_CHECK(none == nullptr);   
+}
+
+//findShortestPath()
+BOOST_AUTO_TEST_CASE(TestShortestPath)
+{
+    MapState map;
+
+    Station* paris = map.getStationByName("Paris");
+    Station* rome  = map.getStationByName("Rome");
+
+    Path p = map.findShortestPath(paris, rome);
+
+    BOOST_REQUIRE(p.STATIONS.size() > 0);
+    BOOST_CHECK_EQUAL(p.STATIONS.front()->data->name, "Paris");
+    BOOST_CHECK_EQUAL(p.STATIONS.back()->data->name, "Rome");
+}
+// test with impossible stations, should always fail
+/*
+BOOST_AUTO_TEST_CASE(TestShortestPath_ImpossiblePath)
+{
+    MapState map;
+    
+    Station dummyA{{}, new StationData{"DummyA", 99}};
+    Station dummyB{{}, new StationData{"DummyB", 100}};
+
+    Path p = map.findShortestPath(&dummyA, &dummyB);
+
+    BOOST_CHECK_EQUAL(p.STATIONS.size(), 0);
+} */
+
+
+//printToString()
+BOOST_AUTO_TEST_CASE(TestPrintToString)
+{
+    MapState map;
+
+    std::string repr = map.printToString();
+
+    BOOST_CHECK(!repr.empty());
+    BOOST_CHECK(repr.find("Paris") != std::string::npos);
+    BOOST_CHECK(repr.find("Roads") != std::string::npos);
+}
+
+
 
 
 /* vim: set sw=2 sts=2 et : */
