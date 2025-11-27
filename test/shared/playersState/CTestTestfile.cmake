fc8fabedc36b26fbb6673295282863ef45bffcee
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-11-27 08:39:05 +0000

    cleaning useless/old files + fixing unix makefile during build

--- test/shared/playersState/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /app/test/shared/playersState
-# Build directory: /app/test/shared/playersState
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(test_players_state "/app/test/shared/playersState/test_players_state")
-set_tests_properties(test_players_state PROPERTIES  WORKING_DIRECTORY "/app" _BACKTRACE_TRIPLES "/app/test/CMakeLists.txt;77;add_test;/app/test/shared/playersState/CMakeLists.txt;2;add_custom_test;/app/test/shared/playersState/CMakeLists.txt;0;")
