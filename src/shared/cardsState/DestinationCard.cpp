f2c03c45172acf627a47827bc85c59b8a313ff09
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-11-28 11:33:32 +0100

    [cards_v1_final]  updating the destination card to implement features of mapstate

--- src/shared/cardsState/DestinationCard.cpp
+++ src/shared/cardsState/DestinationCard.cpp
@@ -2,19 +2,25 @@
 #include <ostream>
 
 #include "DestinationCard.h"
+#include "mapState/Station.h"
+
+
+
 namespace cardsState {
-    DestinationCard:: DestinationCard (std::string cityA, std::string cityB, int points){
-        this->cityA=cityA;
-        this->cityB=cityB;
-        this->points=points;
-    }
-   std::string DestinationCard::getCityA () {return this->cityA;}
-    std::string DestinationCard::getCityB () {return this->cityB;}
-    int DestinationCard::getPoints () {return this->points;}
-    void DestinationCard::display() {
-        std::cout << "Destination from " << cityA << " to " << cityB
-                  << " worth " << points << " points." << std::endl;
+
+    DestinationCard::DestinationCard(mapState::Station* stationA, mapState::Station* stationB, int points)
+      : stationA(stationA), stationB(stationB), points(points) {}
+
+    mapState::Station* DestinationCard::getstationA() {
+        return stationA;
     }
 
+    mapState::Station* DestinationCard::getstationB() {
+        return stationB;
+    }
+
+    int DestinationCard::getPoints() {
+        return points;
+    }
 
 }
