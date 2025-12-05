633b4b549f20675053f3c57c605c0aaa1edf4c67
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-12-05 12:09:42 +0100

    [cards_v1_final]  adding the function takeCard in playerCards

--- src/shared/cardsState/PlayerCards.cpp
+++ src/shared/cardsState/PlayerCards.cpp
@@ -1,6 +1,6 @@
 #include "PlayerCards.h"
 #include <iostream>
-
+#include "SharedDeck.h"
 namespace cardsState {
   PlayerCards::PlayerCards(std::vector<DestinationCard>* destinationCards, std::vector<WagonCard>* wagonCards) {
       this->destinationCards = std::make_shared<Deck<DestinationCard>>(destinationCards);
@@ -11,5 +11,14 @@ namespace cardsState {
         destinationCards->display();
         std::cout << " Player's Wagon Cards: " << std::endl;
          wagonCards->display();
+  }
+
+    template<class CardType> void PlayerCards::takeCard(int number) {
+    cardsState::SharedDeck<CardType>::drawCard(number, this);
+
+  }
+    template<class CardType> void PlayerCards::takeCard(CardType* card ) {
+    cardsState::SharedDeck<CardType>::drawCard(card, this);
+
   }
 }
