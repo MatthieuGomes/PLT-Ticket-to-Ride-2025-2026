3ebd14ee3d8da77b68738318d9120c803e114867
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-12-05 16:04:25 +0000

    cleaning tests in general

--- test/shared/cardsState/test_cards_state.cpp
+++ test/shared/cardsState/test_cards_state.cpp
@@ -13,24 +13,9 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
 BOOST_AUTO_TEST_CASE(TestExemple)
 {
   {
-    // ExampleA exA;
-    // exA.setX(53);
-    // Example ex;
-    // ex.setA(exA);
-    // BOOST_CHECK_EQUAL(ex.getA().x, 53);
-    // exA.setX(21);
-    // BOOST_CHECK_EQUAL(ex.getA().x, 53);
-    // ex.setA(exA);
-    // BOOST_CHECK_EQUAL(ex.getA().x, 21);
   }
 
   {
-    // ExampleA exA;
-    // exA.setX(21);
-    // Example ex;
-    // ex.setA(exA);
-    // BOOST_CHECK_LE(ex.getA().x, 32); // Less than equal
-    // BOOST_CHECK_GT(ex.getA().x, 11); // Greater than equal
   }
 }
 
