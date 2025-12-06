0ea9806734ef7a4b927abf96e6a689fb00b69f8c
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-12-06 09:57:57 +0100

    [cards_v1_final]  testing the destinationcard class

--- test/shared/cardsState/Card/test_DestinationCard.cpp
+++ test/shared/cardsState/Card/test_DestinationCard.cpp
@@ -12,12 +12,24 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
 
 BOOST_AUTO_TEST_CASE(TestDestinationCard) {
   {
+    DestinationCard card("paris","rome",12);
+
+    BOOST_CHECK_EQUAL(card.getstationA(), "paris");
   }
+
   {
+
+    DestinationCard card("paris","rome",12);
+    BOOST_CHECK_EQUAL(card.getPoints(), 12);
+
   }
+
+
   {
-  }
-  {
+    DestinationCard card("paris","rome",12);
+    BOOST_CHECK_EQUAL(card.getstationB(), "rome");
+
+
   }
 }
 
