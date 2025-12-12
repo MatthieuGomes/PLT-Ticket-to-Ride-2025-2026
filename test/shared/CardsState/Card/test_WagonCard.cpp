d541c339b7234649c609125547ec65d12b9d934c
MatthieuGomes <matthieu.gomes@ensea.fr>
2025-12-12 06:36:07 +0000

    [new_dev] normalization of tests

--- test/shared/CardsState/Card/test_WagonCard.cpp
+++ test/shared/CardsState/Card/test_WagonCard.cpp
@@ -3,7 +3,6 @@
 
 #include "../../src/shared/cardsState/CardsState.h"
 
-
 using namespace ::cardsState;
 
 BOOST_AUTO_TEST_CASE(TestStaticAssert)
@@ -11,25 +10,46 @@ BOOST_AUTO_TEST_CASE(TestStaticAssert)
   BOOST_CHECK(1);
 }
 
-BOOST_AUTO_TEST_CASE(TestWagonCard)
+BOOST_AUTO_TEST_SUITE(Constructor)
+
+BOOST_AUTO_TEST_CASE(Constructor)
 {
   {
     WagonCard card(ColorCard::RED);
     BOOST_CHECK_EQUAL(card.color, ColorCard::RED);
-
   }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(GettersAndSetters)
+
+BOOST_AUTO_TEST_SUITE(Getters)
+
+BOOST_AUTO_TEST_CASE(GetColor)
+{
   {
-    WagonCard card(ColorCard::BLUE);
-    BOOST_CHECK_EQUAL(card.getColor(), ColorCard::BLUE);
+    WagonCard card(ColorCard::RED);
+    BOOST_CHECK_EQUAL(card.getColor(), ColorCard::RED);
   }
+}
 
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Setters)
+
+BOOST_AUTO_TEST_CASE(SetColor)
+{
   {
     WagonCard card(ColorCard::RED);
     card.setColor(ColorCard::BLUE);
     BOOST_CHECK_NE(card.getColor(), ColorCard::RED);
     BOOST_CHECK_EQUAL(card.getColor(), ColorCard::BLUE);
-
   }
 }
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
 
 /* vim: set sw=2 sts=2 et : */
---
+++ test/shared/CardsState/Deck/test_Deck.cpp
@@ -0,0 +1,45 @@
+
+#include <boost/test/unit_test.hpp>
+
+#include "../../src/shared/cardsState/CardsState.h"
+#include "../../src/shared/cardsState/Deck.h"
+
+using namespace ::cardsState;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+    BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_SUITE(Constructors)
+BOOST_AUTO_TEST_CASE(Default)
+{
+    {
+        ColorCard cardArgs[] = {ColorCard::RED, ColorCard::BLUE};
+        WagonCard card1(cardArgs[0]);
+        WagonCard card2(cardArgs[1]);
+        auto *cardsVec = new std::vector<WagonCard>{card1, card2};
+        Deck<WagonCard> deck(cardsVec);
+        BOOST_CHECK_EQUAL(deck.countCards(), sizeof(cardArgs) / sizeof(ColorCard));
+        for (unsigned int i = 0; i < static_cast<int>(sizeof(cardArgs)) / sizeof(ColorCard); i++)
+        {
+            BOOST_CHECK_EQUAL(deck.cards[i]->color, cardArgs[i]);
+        }
+    }
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(GettersAndSetters)
+
+BOOST_AUTO_TEST_SUITE(Getters)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Setters)
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE_END()
+
+/* vim: set sw=2 sts=2 et : */
