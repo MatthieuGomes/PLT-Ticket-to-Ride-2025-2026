30024821d4b2e3df57ae74760ed52869d11aed08
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-12-19 13:04:28 +0100

    [cards_v1_final]  testing the playercards constructor

---
+++ test/shared/CardsState/PlayerCards/test_PlayerCards.cpp
@@ -0,0 +1,50 @@
+#include <boost/test/unit_test.hpp>
+#include <memory>
+#include <vector>
+
+#include "../../src/shared/cardsState/PlayerCards.h"
+#include "../../src/shared/cardsState/CardsState.h"
+#include "../../src/shared/cardsState/DestinationCard.h"
+#include "../../src/shared/cardsState/WagonCard.h"
+
+using namespace ::cardsState;
+
+BOOST_AUTO_TEST_CASE(TestStaticAssert)
+{
+    BOOST_CHECK(1);
+}
+
+BOOST_AUTO_TEST_SUITE(Constructors)
+
+BOOST_AUTO_TEST_CASE(Constructor_WithSharedPtrVectors)
+{
+    std::vector<std::shared_ptr<DestinationCard>> destinationCards;
+    std::vector<std::shared_ptr<WagonCard>> wagonCards;
+
+    auto d1 = std::make_shared<DestinationCard>("Paris", "Lyon", 5);
+    auto w1 = std::make_shared<WagonCard>(ColorCard::RED);
+
+    destinationCards.push_back(d1);
+    wagonCards.push_back(w1);
+
+    PlayerCards player(destinationCards, wagonCards);
+
+    BOOST_CHECK(player.destinationCards);
+    BOOST_CHECK(player.wagonCards);
+    BOOST_CHECK_EQUAL(player.destinationCards->countCards(), 1);
+    BOOST_CHECK_EQUAL(player.wagonCards->countCards(), 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Operations)
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+BOOST_AUTO_TEST_SUITE(Interactions)
+
+
+BOOST_AUTO_TEST_SUITE_END()
+
+/* vim: set sw=2 sts=2 et : */
