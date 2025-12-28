144bdfee85e7f6ef940061ce7b4251297a6bd035
Chahrazed baatout <baatoutchahrazed@gmail.com>
2025-12-28 17:40:11 +0100

    [cards_v1_final]  updating the cards with Station attributes #working_in_progress

--- src/shared/cardsState/DestinationCard.cpp
+++ src/shared/cardsState/DestinationCard.cpp
@@ -7,27 +7,31 @@
 
 namespace cardsState {
 
-    DestinationCard::DestinationCard(std::string stationA, std::string stationB, int points)
+    DestinationCard::DestinationCard(std::shared_ptr<mapState::Station> stationA, std::shared_ptr<mapState::Station> stationB, int points)
       : stationA(stationA), stationB(stationB), points(points) {}
 
-    std::string DestinationCard::getstationA() {
+
+
+    std::shared_ptr<mapState::Station> DestinationCard::getstationA() {
         return stationA;
     }
-
-    std::string DestinationCard::getstationB() {
+    std::shared_ptr<mapState::Station> DestinationCard::getstationB() {
         return stationB;
     }
 
     int DestinationCard::getPoints() {
         return points;
     }
-/*
+
     void DestinationCard::display() {
-        std::cout << "Destination Card: " << stationA->data->name << " to " << stationB->data->name
-                  << " for " << points << " points." << std::endl;
-    }*/
-    void DestinationCard::display() {
-        std::cout << "Destination Card: " << stationA << " to " << stationB
-                  << " for " << points << " points." << std::endl;
+        std::cout << "Destination Card:" << std::endl;
+
+        std::cout << "From: ";
+        stationA->display();
+
+        std::cout << "To: ";
+        stationB->display();
+
+        std::cout << "Points: " << points << std::endl;
     }
 }
