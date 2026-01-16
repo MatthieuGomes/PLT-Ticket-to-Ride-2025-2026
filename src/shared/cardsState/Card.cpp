2a5730913ee1a0b918c2bf73f47628b0eaa8517a
MatthieuGomes <matthieu.gomes@ensea.fr>
2026-01-16 09:40:15 +0000

    [new_dev] fixing message from destorying card

--- src/shared/cardsState/Card.cpp
+++ src/shared/cardsState/Card.cpp
@@ -1,11 +1,19 @@
 #include "Card.h"
 
 #include <iostream>
+#define DEBUG_MODE false
+#if DEBUG_MODE == true
+#define DEBUG
+#define DEBUG_PRINT(x) std::cout << x << std::endl
+#else
+#define DEBUG_PRINT(x)
+#endif
 
+namespace cardsState
+{
 
-namespace cardsState {
-
-    Card::~Card() {
-        std::cout<<"Destroying Card"<< std::endl;
-    };
+    Card::~Card()
+    {
+        DEBUG_PRINT("Destroying Card");
+    }
 }
