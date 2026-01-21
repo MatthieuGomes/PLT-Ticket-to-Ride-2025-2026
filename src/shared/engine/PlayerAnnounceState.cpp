63e86c82bf56d682db93f16149f9b02597d88175
fetohiaras <theo.farias@gmail.com>
2026-01-21 01:06:43 +0000

    [engine] added playerannouncestate draft

---
+++ src/shared/engine/PlayerAnnounceState.cpp
@@ -0,0 +1,60 @@
+#include "PlayerAnnounceState.h"
+
+#include <iostream>
+
+#include "Engine.h"
+#include "PlayerTurnState.h"
+#include "StateMachine.h"
+#include "playersState/Player.h"
+
+namespace engine
+{
+  namespace
+  {
+    const int kDefaultPlayerIndex = 0;
+  }
+
+  void PlayerAnnounceState::onEnter(std::shared_ptr<Engine> engine)
+  {
+    if (!engine)
+    {
+      return;
+    }
+
+    engine->phase = Phase::PLAYER_ANNOUNCE;
+
+    std::shared_ptr<state::State> state = engine->getState();
+    std::string playerName = "Player";
+    int index = engine->context.currentPlayer;
+
+    if (state)
+    {
+      std::vector<std::shared_ptr<playersState::Player>> players = state->players.getPlayers();
+      if (players.empty())
+      {
+        playerName = "Player";
+        engine->context.currentPlayer = kDefaultPlayerIndex;
+      }
+      else
+      {
+        if (index < 0 || index >= static_cast<int>(players.size()))
+        {
+          index = kDefaultPlayerIndex;
+          engine->context.currentPlayer = index;
+        }
+        if (players[index])
+        {
+          playerName = players[index]->getName();
+        }
+      }
+    }
+
+    std::cout << "It's " << playerName << "'s turn!" << std::endl;
+
+    if (engine->stateMachine)
+    {
+      std::shared_ptr<GameState> nextState(new PlayerTurnState());
+      engine->stateMachine->transitionTo(engine, nextState);
+    }
+  }
+}
